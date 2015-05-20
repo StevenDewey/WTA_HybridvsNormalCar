bd22b20364f0597b8f8a2bd7d98694bf8311176e
Matt Edwards <edwardsjm01@gmail.com>
2015-05-19 23:46:05 -0600

    get html hooked up with javascript and get resell value to work

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 'use strict';
 
-angular.module('HybridApp',[]);
+angular.module('HybridApp',[]).directive('hybridVsNormal',[function(){
+	return {
+		restrict:'A',
+		link:function($scope){
+
+			function setup(){
+				$scope.hybrid = {};
+				$scope.normal = {};
+				$scope.type = 'cost';
+				$scope.percents =[];
+				$scope.resellValue = 75;
+			}
+
+			$scope.calc = function(type) {
+				switch(type){
+					case 'gas':
+						alert('You select type ' + type);
+						break;
+					case 'cost':
+						alert('You select type ' + type);
+						break;
+					default:
+						alert('please select type');
+				}
+			};
+
+			setup();
+
+			for(var i=100;i>49;i-=5){
+				$scope.percents.push(i);
+			}
+		}
+	};
+}]);
